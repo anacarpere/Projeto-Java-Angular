144031b619e22080850f99b4c0b339d721c605f2
anacarpere <86481653+anacarpere@users.noreply.github.com>
2022-04-28 18:02:48 -0300

    Inclusão de criptografia de senha e métodos usuário

---
+++ Services/src/main/java/SoulCode/Services/Repositories/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package SoulCode.Services.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import SoulCode.Services.Models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+	
+	Usuario findByLogin(String login);
+	
+}
